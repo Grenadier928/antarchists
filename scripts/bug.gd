867ec7fcc5a83f0cf30ef95a2b10b1e37a13b144
Nicholas <nicholasgeren@gmail.com>
2024-10-04 19:39:53 -0400

    more simple changes

---
+++ scripts/bug.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var health = null
+var speed = null
+var evasion = null
+var strength = null
+
+var status_armored = 0
+var status_flying = 0
+var status_blinded = 0
+
+
+func spawn_bug(params):
+	pass
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
