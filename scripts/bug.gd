ab7779f5cd0ece34c94a9c78b02eb0b23eb6637c
Nicholas <nicholasgeren@gmail.com>
2024-10-04 22:14:45 -0400

    aaaa

--- scripts/bug.gd
+++ scripts/bug.gd
@@ -5,16 +5,37 @@ var speed = null
 var evasion = null
 var strength = null
 
+var custom_name = "Antony"
+
+var sprite_path = "basic_brown_ant.png"
+
 var status_armored = 0
 var status_flying = 0
 var status_blinded = 0
 
+var in_combat = false
+
+var attacks_list = [];
+
+
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if(event.is_pressed()):
+			if in_combat:
+				var combat = get_node("/root/Index/combat")
+				#$selection.visible = true
+				
+				#print("pressed!")
+			
 
 func spawn_bug(params):
 	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$selection.visible = false
+	$Sprite2D.texture = load("res://textures/bug_sprites/" + sprite_path)
 	pass # Replace with function body.
 
 
--- scripts/index_manager.gd
+++ scripts/index_manager.gd
@@ -11,16 +11,24 @@ var CURRENT_PARTY = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var test_ant = basic_bug.instantiate()
+	test_ant.speed = 1
 	CURRENT_PARTY.append(test_ant)
 	#print("Asdsdsds")
 	var current_combat = basic_combat_scene.instantiate()
 	var scene_root = get_node("/root/Index/")
 	scene_root.add_child(current_combat)
+	
+	
 	current_combat.spawnPlayerTeam(CURRENT_PARTY)
+	
+	var evil_ant = basic_bug.instantiate()
+	evil_ant.speed = 2
+	current_combat.spawnEnemyTeam([evil_ant])
+	current_combat.initCombatQueue()
 	#print("B")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	pass
